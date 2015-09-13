5454326ca65c44e7105f9c13be31996159df3fe4
Raquel Lemos <raqlem@gmail.com>
2015-09-13 20:16:32 -0300

    Cria novo projeto

---
+++ app/views/produtos/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @produto, :id, :nome, :descricao, :valor, :imagem_ur, :created_at, :updated_at
---
+++ db/migrate/20150913213759_create_produtos.rb
@@ -0,0 +1,12 @@
+class CreateProdutos < ActiveRecord::Migration
+  def change
+    create_table :produtos do |t|
+      t.string :nome
+      t.text :descricao
+      t.decimal :valor
+      t.string :imagem_ur
+
+      t.timestamps null: false
+    end
+  end
+end
